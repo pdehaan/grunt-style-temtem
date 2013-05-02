4c89be03a76bc6b1e929379c483ef50cdfbd4cc6
ikumi.toshihiko <ikumi.toshihiko@dena.jp>
2013-05-02 15:32:35 +0900

    update

--- tasks/style_temtem.js
+++ tasks/style_temtem.js
@@ -7,110 +7,52 @@
  */
 
 'use strict';
-var fs = require("fs"),
-    jsdom = require('jsdom').jsdom,
+var jsdom = require('jsdom').jsdom,
     md = require("node-markdown").Markdown,
     ejs = require('ejs');
+
+var tmpCssCopyFile = 'tmp/tmp_css_copy_file.scss',
+    tmpCssFile = 'tmp/tmp_css_file.css',
+    tmpScssFile = 'tmp/tmp_scss_file.scss';
 module.exports = function(grunt) {
-    var req = 
     grunt.registerTask('style_temtem', 'discription', function() {
         var options = this.options({
-            selector : {
-                header: 'style-temtem-header',
-                template: 'style-temtem-template'
-            }
+            preprocessor : 'scss'
         });
         var config = grunt.config('style_temtem'),
-            cssArr = config.files,
-            cssPath, tempPath, resultPath, filesrc, parts, htmlParts;
+            cssArr = config.files;
 
         cssArr.forEach(function(fileObj) {
-            htmlParts = [];
-            cssPath = fileObj.css;
-            tempPath = fileObj.temp;
-            resultPath = fileObj.result;
-            filesrc = grunt.file.read(cssPath);
-            var req = filesrc.match();
-            // console.log('0 ****************');
-            // console.log(req[0]);
-            // console.log('1 ****************');
-            // console.log(req[1]);
-            // console.log('2 ****************');
-            // console.log(req[2]);
-            // console.log('3 ****************');
-            // console.log(req[3]);
+            var htmlParts = [],
+                cssPath = fileObj.css,
+                tempPath = fileObj.temp,
+                resultPath = fileObj.result,
+                filesrc = grunt.file.read(cssPath),
+                cssText = "@import \"" + tmpCssCopyFile.split('tmp/').pop() + "\"",
+                parts;
+            
+            grunt.file.write(tmpCssCopyFile, grunt.file.read(cssPath));
             filesrc.match(/\/\*[^`]+[\n.]`{3}[^`]*`{3}[^`]*\*\//g).forEach(function(src) {
                 parts = src.match(/\/\*([^`]+)[\n.]`{3}([^`]*)`{3}([^`]*)\*\//);
-                // console.log('src ****************');
-                // console.log(src);
-                // console.log("0 ****************");
-                // console.log(parts[0]);
-                // console.log("1 ****************");
-                // console.log(parts[1]);
-                // console.log("2 ****************");
-                // console.log(parts[2]);
-                // console.log("3 ****************");
-                // console.log(parts[3]);
                 htmlParts.push({
                     header : md(parts[1]),
                     template : parts[2],
                     method : parts[3],
                 });
+                cssText += parts[3];
             });
-
-            // var tempSrc = jsdom(grunt.file.read(tempPath));
-            // htmlParts.forEach(function(partsObj) {
-            //     var headerElm = tempSrc.getElementsByClassName(options.selector.header),
-            //         templateElm = tempSrc.getElementsByClassName(options.selector.template),
-            //         counter = 0,
-            //         prop;
-                
-            //     for (prop in headerElm) {
-            //         headerElm[prop].innerHTML = partsObj.header;
-            //     }
-            //     console.log(tempSrc.innerHTML);                
-            // });
 
             var outputSrc = ejs.render(grunt.file.read(tempPath), {
                 items : htmlParts
             });
             grunt.file.write(resultPath, outputSrc);
-            console.log(outputSrc);
+            
+            if (options.preprocessor === 'scss') {
+                grunt.file.write(tmpScssFile, cssText);
+            }else {
+                grunt.file.write(tmpCssFile, cssText);
+            }        
         });
     });
-
-    // grunt.registerMultiTask('style_temtem', 'Your task description goes here.', function() {
-    //   // Merge task-specific and/or target-specific options with these defaults.
-    //   var options = this.options({
-    //     punctuation: '.',
-    //     separator: ', '
-    //   });
-
-    //   // Iterate over all specified file groups.
-    //   this.files.forEach(function(f) {
-    //     // Concat specified files.
-    //     var src = f.src.filter(function(filepath) {
-    //       // Warn on and remove invalid source files (if nonull was set).
-    //       if (!grunt.file.exists(filepath)) {
-    //         grunt.log.warn('Source file "' + filepath + '" not found.');
-    //         return false;
-    //       } else {
-    //         return true;
-    //       }
-    //     }).map(function(filepath) {
-    //       // Read file source.
-    //       return grunt.file.read(filepath);
-    //     }).join(grunt.util.normalizelf(options.separator));
-
-    //     // Handle options.
-    //     src += options.punctuation;
-
-    //     // Write the destination file.
-    //     grunt.file.write(f.dest, src);
-
-    //     // Print a success message.
-    //     grunt.log.writeln('File "' + f.dest + '" created.');
-    //   });
-    // });
 
 };
